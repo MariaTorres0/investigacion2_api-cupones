6c760d9d5bce68479176551b46653954c3d7deb1
MariaTorres0 <mariatorres11001@gmail.com>
2023-05-09 16:21:59 -0600

    cambiando validaciones cupones

--- app/Http/Controllers/CuponController.php
+++ app/Http/Controllers/CuponController.php
@@ -18,7 +18,7 @@ class CuponController extends Controller
         return response()->json([
             'status' => 200,
             'body' => $cupon
-        ]);
+        ], 200);
     }
 
     public function updateCupon($id)
@@ -26,21 +26,21 @@ class CuponController extends Controller
 
         $cupon = Cupon::where('id_cupon', '=', $id)->get();
 
-        if ($cupon != null) {
+        if ($cupon->isNotEmpty()) {
             if ($cupon[0]->vencido != 0) {
                 return response()->json([
                     'status' => 400,
                     'msj' => 'Cupon vencido',
                     'body' => null,
 
-                ]);
+                ], 400);
             } else if ($cupon[0]->estado == 1) {
                 return response()->json([
                     'status' => 400,
                     'msj' => 'Cupon ya canjeado',
                     'body' => null,
 
-                ]);
+                ], 400);
             } else {
                 $cuponUpd = Cupon::where('id_cupon', $id)->update(['estado' => 1]);
                 $cupon = Cupon::where('id_cupon', '=', $id)->get();
@@ -48,7 +48,7 @@ class CuponController extends Controller
                     'status' => 200,
                     'msj' => 'Actualizado',
                     'body' => $cupon,
-                ]);
+                ], 200);
             }
 
         } else {
@@ -56,7 +56,7 @@ class CuponController extends Controller
                 'status' => 404,
                 'msj' => 'cupon no encontrado',
                 'body' => $cupon
-            ]);
+            ], 404);
         }
 
     }
